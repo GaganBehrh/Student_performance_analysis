0e44835703832d8dcca0b108007fb3e54037af9d
Gagi Behrh <gaganpreet.behrh3@gmail.com>
2022-08-08 08:49:06 +0000

    Max score found

--- run.py
+++ run.py
@@ -15,7 +15,7 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Student_analysis')
 
 
-def get_students_marks():
+def getStudentsMarks():
     """
     Gets the marks of the student
     """
@@ -23,16 +23,16 @@ def get_students_marks():
         print("Please enter the marks for 5 students in spanish subject")
         print("Data should be 5 numbers, separated by commas.")
         print("Example: 20,30,40,50,60\n")
-        data_str = input("Enter your data here: ")
-        sales_data = data_str.split(",")
+        dataStr = input("Enter your data here: ")
+        salesData = dataStr.split(",")
 
-        if validate_data(sales_data):
+        if validateData(salesData):
             print("Data is valid!")
             break
 
-    return sales_data
+    return salesData
 
-def validate_data(values):
+def validateData(values):
     """
     """
     try:
@@ -45,18 +45,19 @@ def validate_data(values):
         return False
     return True
 
-def update_student_worksheet(data,worksheet):
+def updateStudentWorksheet(data,worksheet):
     print(f"Updating the worksheet {worksheet}")
     update_worksheet=SHEET.worksheet(worksheet)
     update_worksheet.append_row(data)
     print(f"{worksheet} updated successfully")
 
 
-def student_total_score():
-    """to get the entries from each column and calculate the percentage of each student"""
+def studentTotalScore():
+    """to get the entries from each column and calculate the sum of marks for each student"""
     student_data=SHEET.worksheet("student").get_all_values()
     print(student_data)
     print("Calculating student total marks")
+    #to get the number of students
     num_students = len(student_data[0])
     new_list = []
     for i in range(0, num_students):
@@ -64,15 +65,27 @@ def student_total_score():
         for j, row in enumerate(student_data):
             if j == 0:
                 continue
+            #to append each row to the student list
             student.append(int(row[i]))
         new_list.append(sum(student))
-    print(new_list)
+    return new_list
+
+def maxScore(scoreList):
+    max=scoreList[0];
+    for score in range(0,len(scoreList)):
+        if(max<scoreList[score]):
+          max=scoreList[score]
+    print(max)         
+   
 
     
 def main():
-    student_spanish_marks=get_students_marks()
-    spanish_data=[int(marks) for marks in student_spanish_marks]
-    update_student_worksheet(spanish_data,"student")
-    student_total_score()
+   # studentSpanishMarks=getStudentsMarks()
+    #spanishData=[int(marks) for marks in studentSpanishMarks]
+    #updateStudentWorksheet(spanishData,"student")
+    totalStudentScore=studentTotalScore()
+    print(totalStudentScore)
+    maxScore(totalStudentScore)
+
 
 main()
