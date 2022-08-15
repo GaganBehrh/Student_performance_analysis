243bc4069e70778543bba61e00d3d69a90166996
Gagi Behrh <gaganpreet.behrh3@gmail.com>
2022-08-15 18:43:06 +0000

    Right student name achieved

--- run.py
+++ run.py
@@ -1,18 +1,20 @@
 import gspread
 from google.oauth2.service_account import Credentials
 from pandas import ExcelWriter
-from pandas import ExcelFile 
+from pandas import ExcelFile
 import pandas as pd
+
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/drive"
-    ]
+]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Student_analysis')
+
 
 def get_students_marks():
     print("Please enter the marks for 5 students individually")
@@ -21,33 +23,34 @@ def get_students_marks():
     data_str3 = int(input("Enter your data here for Racheal: "))
     data_str4 = int(input("Enter your data here for Monica: "))
     data_str5 = int(input("Enter your data here for Christine: "))
-    sales_data=[]
+    sales_data = []
     sales_data.append(data_str1)
     sales_data.append(data_str2)
     sales_data.append(data_str3)
     sales_data.append(data_str4)
     sales_data.append(data_str5)
     print(f"The data you entered is {sales_data}")
-    return sales_data         
+    return sales_data
 
-def update_student_worksheet(data,worksheet):
+
+def update_student_worksheet(data, worksheet):
     """
     to update the worksheet
     """
-    flag=False
-    update_worksheet=SHEET.worksheet(worksheet)
+    flag = False
+    update_worksheet = SHEET.worksheet(worksheet)
     for entries in data:
-        if(entries>0 and entries<100 ):
-            flag=True
+        if(entries > 0 and entries < 100):
+            flag = True
         else:
-            flag=False
+            flag = False
             print("Invalid values added, it should lie in betwwen zero and humdred, please restart and the datasheet wont be updated")
             break
-    if(flag==True):
+    if(flag == True):
         print("You entered the valid values")
         update_worksheet.append_row(data)
         print("Hence,Here is the updated data")
-        student_data=SHEET.worksheet("student").get_all_values()
+        student_data = SHEET.worksheet("student").get_all_values()
         print(student_data)
 
 
@@ -55,7 +58,7 @@ def show_data():
     """
     to show the existing data
     """
-    student_data=SHEET.worksheet("student").get_all_values()
+    student_data = SHEET.worksheet("student").get_all_values()
     print("Here is the existing data")
     print(student_data)
 
@@ -65,9 +68,9 @@ def student_total_score():
     to get the entries from each column and calculate the sum of marks for each student
     """
     print("Here are the total marks obtained by each student")
-    grade=''
-    #to get the number of students
-    student_data=SHEET.worksheet("student").get_all_values()
+    grade = ''
+    # to get the number of students
+    student_data = SHEET.worksheet("student").get_all_values()
     num_students = len(student_data[0])
     print(student_data[0])
     new_list = []
@@ -79,42 +82,46 @@ def student_total_score():
             student.append(int(row[i]))
         new_list.append(sum(student))
     print(new_list)
-    max=new_list[0]
-    name=""
-    maxScoreIndex=0
-    for students in student_data[0]:
-        for score in range(0,len(new_list)):
-            if(max<=new_list[score]):
-                max=new_list[score]
-                avg=max/(len(student))
-                name=students
-                maxScoreIndex=score+1
-                if(avg<65):
-                    grade='C'
-                elif(avg<75 and avg>65):
-                    grade='B'
+    max = new_list[0]
+    name = ""
+    maxScoreIndex = 0
+    students = student_data[0]
+    for student_name in students:
+        for score in range(0, len(new_list)):
+            if(max <= new_list[score]):
+                max = new_list[score]
+                avg = max/(len(student))
+                name = student_name
+                maxScoreIndex = score
+                if(avg < 65):
+                    grade = 'C'
+                elif(avg < 75 and avg > 65):
+                    grade = 'B'
                 else:
-                    grade='A'
-    print(f"Here are the details of the topper of the class: {name},student{maxScoreIndex} Max score :{max}, Percentage:{round(avg)}%, Grade:{grade}")
-        
-          
-    
+                    grade = 'A'
+    print(
+        f"Here are the details of the topper of the class:\n{students[maxScoreIndex]} Max score :{max}, Percentage:{round(avg)}%, Grade:{grade}")
+
+
 def main():
     print("Welcome to Student performance analysis")
     print("What would like to perform?")
     print("1. See the existing Marksheet")
     print("2. Get Averages/percentage for the students and know the grade of the student")
     print("3. Enter the data and show the updated results")
-    choice=int(input("Please Enter your choice"))
-    total_student_score=[]
-    if(choice==1):
-        show_data()  
-    elif (choice==2):
+    choice = int(input("Please Enter your choice \n"))
+    total_student_score = []
+
+    if(choice == 1):
+        show_data()
+    elif (choice == 2):
         student_total_score()
-    elif (choice==3):
-        student_spanish_marks=get_students_marks()
-        spanish_data=[int(marks) for marks in  student_spanish_marks]
-        update_student_worksheet(spanish_data,"student")
+    elif (choice == 3):
+        student_spanish_marks = get_students_marks()
+        spanish_data = [int(marks) for marks in student_spanish_marks]
+        update_student_worksheet(spanish_data, "student")
     else:
-        print("Invalid choice, it has to be in between 1 and 3, no zeros or negative values are allowed")  
+        print("Invalid choice, it has to be in between 1 and 3, no zeros or negative values are allowed")
+
+
 main()
